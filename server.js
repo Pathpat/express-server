053c7e3c4bda04cc187cc5d5306791f26092451b
Pathpat <xristospatakias@gmail.com>
2023-08-08 12:00:45 +0300

    Create friends list and route parameters based on id

--- server.js
+++ server.js
@@ -4,12 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req,res) =>{
-    res.send({id: 0,
-              name: "Linus Torvalds"
-            });
+const friends = [
+    {  
+    id: 0,
+    name: "Linus Torvalds"
+    },
+    {
+    id: 1,
+    name: "Edward Diaz"
+    },
+    {
+    id: 2,
+    name: "Issac Newton"
+    }
+];
+
+app.get('/friends', (req,res) =>{
+    res.json(friends);
 })
 
+app.get('/friends/:friendId', (req,res) =>{
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    }else {
+        res.status(404).json({
+            error: "Friend not found"
+        })
+    }
+});
 
 app.get('/messages', (req,res) =>{
     res.send('<ul><li>Hellooooworld</li></ul>');
