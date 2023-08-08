5fd0e5ed1fb2665e42ac5f9563feecf2ff59055f
Pathpat <xristospatakias@gmail.com>
2023-08-08 14:45:41 +0300

    Express Routers

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const express = require('express');
-const messagesControllers = require ('./controllers/messages.controller');
-const friendsControllers = require ('./controllers/friends.controller');
+
+const messagesRouter = require('./routes/messages.router');
+const friendsRouter = require('./routes/friends.router');
 
 const app = express();
 
 const PORT = 3000;
 
-
-
 app.use((req,res,next)=>{
     const start = Date.now();
     next();
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends',friendsControllers.postFriends);
-app.get('/friends',friendsControllers.getFriends );
-app.get('/friends/:friendId',friendsControllers.getFriendsbyId );
-
-app.get('/messages',messagesControllers.getMessages);
-app.post('/messages',messagesControllers.postMessage);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT,() => {
     console.log(`Listening on port ${PORT}...`);
